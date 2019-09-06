e2dafd6705c3c10228c563bb8b4200ef836dbe45
hangxin.yang <hangxin.yang@xiaobao100.com>
2019-09-06 14:39:27 +0800

    哈哈，终于

---
+++ README.md
@@ -0,0 +1,23 @@
+### Installation
+---
+npm install agent-cli -g
+
+
+### use
+
+agent-cli vue [args...]
+
+agent-cli react [args...]
+
+### tips
+
+if you do not have vue-cli or react-build ,i can help install ,and you can use directive like 
+
+```javascript
+  agent-cli react init my-react   
+```
+
+so i can help your install and open cli to create your project
+
+
+
--- index.js
+++ index.js
@@ -1,73 +1,92 @@
- /**
-  * Module dependencies.
-  */
+#!/usr/bin/env node
 
- var program = require('commander');
- var child_process = require('child_process');
+//上面告诉他是用node去跑这个包
+/**
+ * Module dependencies.
+ */
 
- function collect(val, memo) {
-     memo.push(val);
-     return memo;
- }
 
- let cliName = '';//脚手架名称
- let allArgs = []; //所有参数
- //使用command 
- program.version('0.1.0').arguments('<arg> [otherArgs...]').action(function (arg, otherArgs) {
+var program = require('commander');
+var child_process = require('child_process');
+
+function collect(val, memo) {
+    memo.push(val);
+    return memo;
+}
+
+let cliName = ''; //脚手架名称
+let allArgs = []; //所有参数
+//使用command 
+program.version('0.1.0').arguments('<arg> [otherArgs...]').action(function (arg, otherArgs) {
     cliName = arg;
-     if (otherArgs) {
-         otherArgs.forEach(function (oDir) {
-             allArgs.push(oDir);
-         });
-     }
- });
+    if (!cliName) {
+        console.log('need input type(vue or react)');
+        process.exit();
+    }
+    if (otherArgs) {
+        otherArgs.forEach(function (oDir) {
+            allArgs.push(oDir);
+        });
+    }
+});
 
- program.parse(process.argv);
+program.parse(process.argv);
 
- console.log(allArgs);
+console.log(allArgs);
 
 
- //查询vue-cli版本
- console.log(`search desktop ${cliName}-cli...`);
- child_process.exec(`${cliName == 'vue'?'vue':'build-react'} --version`, (error, stdout, stderr) => {
-     console.log(error, stdout, stderr);
-     if (error) {
-         console.log(`not find ${cliName}-cli`);
-         //install vue-cli
-         installCli().then(() => {
-             console.log('成功跑到这里');
-             useCli();
-         })
-     } else {
-         useCli();
-     }
- })
+//查询vue-cli版本
+console.log(`search desktop ${cliName}-cli...`);
+child_process.exec(`${cliName == 'vue'?'vue':'build-react'} --version`, (error, stdout, stderr) => {
+    console.log(error, stdout, stderr);
+    if (error) {
+        console.log(`not find ${cliName}-cli`);
+        //install vue-cli
+        installCli().then(() => {
+            console.log('install success');
+            useCli();
+        })
+    } else {
+        useCli();
+    }
+})
 
- //安装vue-cli
+//安装vue-cli
 
- function installCli() {
-     return new Promise((resolve, reject) => {
-         console.log(`open install ${cliName}-cli`);
-         child_process.exec(cliName == 'vue'?'npm install -g @vue/cli':'npm install -g build-react', (error, stdout, stderr) => {
-             console.log(error, stdout, stderr);
-             //安装成功
-             if (error) {
-                 console.log('install failed');
-             } else {
-                 console.log(`install success ${cliName}-cli version ${stdout}`)
-                 resolve();
-             }
-         })
-     })
- }
+function installCli() {
+    return new Promise((resolve, reject) => {
+        console.log(`open install ${cliName}-cli`);
+        var ls = child_process.exec(cliName == 'vue' ? 'npm install -g @vue/cli' : 'npm install -g build-react', (error, stdout, stderr) => {
+            console.log(error, stdout, stderr);
+            //安装成功
+            if (error) {
+                console.log('install failed');
+            } else {
+                console.log(`install success ${cliName}-cli version ${stdout}`)
+                resolve();
+            }
+        })
+        ls.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
 
- //使用vue-cli进行操作
- function useCli() {
-     console.log(`open ${cliName}-cli...`);
-     console.log(`start ${cliName} ${allArgs.join(' ')}`);
-    child_process.exec(`start ${cliName == 'vue'?'vue':'build-react'} ${allArgs.join(' ')}`, (error, stdout, stderr) => {});//这里不对
-     setTimeout(() => {
-         console.log('please create your dir');
-         process.exit();
-     }, 200);
- }
+        ls.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+
+        ls.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    })
+}
+
+//使用vue-cli进行操作
+function useCli() {
+    console.log(`open ${cliName}-cli...`);
+    console.log(`start ${cliName} ${allArgs.join(' ')}`);
+    child_process.exec(`start ${cliName == 'vue'?'vue':'build-react'} ${allArgs.join(' ')}`, (error, stdout, stderr) => {}); //这里不对
+    setTimeout(() => {
+        console.log('please create your dir');
+        process.exit();
+    }, 200);
+}
